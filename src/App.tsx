424bede57b04afd6ab8157f164e2a992dcfffda4
vcerut <97165687+vcerut@users.noreply.github.com>
2023-12-07 11:00:34 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Suspense, lazy } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+
+const Homepage = lazy(() => import("./pages/HomePage"));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    //wildcard match, percorso sempre da inserire per ultimo, nel caso in cui nessun altro path venga eseguito
+    path: "*",
+    // element: <h1>404</h1>, //pagina di errore personalizzata, si può anche creare una pagina a parte per poi importarla
+    element: <Navigate to="/" />, //fallback redirect
+  },
+]);
+
+function App() {
+  return (
+    <Suspense fallback={<p>Waiting for lazy load</p>}>
+      {" "}
+      <RouterProvider router={router} />
+    </Suspense>
+  );
+}
+
+export default App;
